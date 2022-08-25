91bfdd7dd1424080945c5d463645528f7e3b681f
TexasMadeKing <JoshBennetsen@gmail.com>
2022-08-25 16:28:52 -0500

    Good Spot

--- src/components/portfolio-container.js
+++ src/components/portfolio-container.js
@@ -8,7 +8,7 @@ export default class PortfolioContainer extends Component {
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <h1>Added from portfolio-container.js</h1>
                 <h2>Portfolio items go here...</h2>
                 <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
             </div>
