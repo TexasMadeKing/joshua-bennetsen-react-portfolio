92bdd37ad9f4788e1da2343cd152c3a446c95daa
TexasMadeKing <JoshBennetsen@gmail.com>
2022-08-25 16:16:04 -0500

    Second Commit

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+import PortfolioContainer from './portfolio-container';
+
+
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Portfolio items go here...</h2>
+            </div>
+        );
+    }
+}
