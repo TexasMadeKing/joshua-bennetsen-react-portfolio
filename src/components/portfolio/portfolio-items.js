24c5b66cd54f17687092869226e8a88ceee3ca14
TexasMadeKing <JoshBennetsen@gmail.com>
2022-08-25 16:57:30 -0500

    Okay

--- src/components/portfolio/portfolio-items.js
+++ src/components/portfolio/portfolio-items.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function() {
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
+
+    console.log("Portfolio container has rendered");
+  }
+
+  portfolioItems() {
+    const data = ["Quip", "Eventbrite", "Ministry Safe", "SwingAway"];
+
+    return data.map(item => {
+      return <PortfolioItem />;
+    });
+  }
+
+  render() {
     return (
-        <div>
-            <h3>Portfolio Item</h3>
-        </div>
+      <div>
+        <h2>Portfolio items go here updated...</h2>
+
+        {this.portfolioItems()}
+      </div>
     );
+  }
 }
